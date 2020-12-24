cc8522156bbfc0979a9063676d9a755ba2d6f925
Aaron Choo <choozy.aaron@gmail.com>
2020-12-24 14:41:58 +0800

    preact-ts: update design for consistency

--- preact-ts/src/components/App.tsx
+++ preact-ts/src/components/App.tsx
@@ -2,9 +2,26 @@ import { h, FunctionalComponent } from "preact";
 
 const App: FunctionalComponent = () => {
   return (
-    <h1 style={{ fontWeight: 400, textAlign: "center" }}>
-      Hello from <code>App.tsx</code>
-    </h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <h1 style={{ fontWeight: 400, color: "rgba(109, 40, 217)", margin: 0 }}>
+        Preact CLI + TypeScript
+      </h1>
+      <p>
+        Edit{" "}
+        <code style={{ color: "rgba(190, 24, 93)" }}>
+          src/components/App.tsx
+        </code>{" "}
+        to start
+      </p>
+    </div>
   );
 };
 
